41b952106ae0cc6baca9e1ab1abd4d87af163ea5
Georges Boris <georgesboris@gmail.com>
2021-01-10 09:10:29 -0300

    feat: elm-ui-docs dev server

--- elm-ui-docs.js
+++ elm-ui-docs.js
@@ -2,10 +2,11 @@
 
 const exec = require("child_process").exec;
 const entryPoint = process.argv[2];
+const port = process.argv[3] || 3000;
 
 if (!entryPoint) {
   console.warn("elm-ui-docs: please specify your entry point.");
   process.exit(1);
 } else {
-  exec(`elm-live ${process.argv[2]} --pushstate --port=3000 --open -- --debug`);
+  exec(`elm-live ${process.argv[2]} --port=${3000} --pushstate --hot --open`);
 }
