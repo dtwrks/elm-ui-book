96b75a9fc6547f7b5688762894082926b67ebbde
Georges Boris <georgesboris@gmail.com>
2021-01-10 08:53:36 -0300

    chore: update elm-ui-docs server

--- elm-ui-docs.js
+++ elm-ui-docs.js
@@ -1,5 +1,9 @@
 #! /usr/bin/env node
 
-const args = process.argv;
+const exec = require("child_process").exec;
 
-console.log(process.argv);
+if (!process.argv[3]) {
+  throw new Error("elm-ui-docs: please specify your entry point.");
+}
+
+exec(`elm-live ${process.argv[3]} --pushstate --port=3000 --open -- --debug`);
