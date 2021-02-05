ca1a820f94e9b7febecc63125f6e1ac70108f213
Georges Boris <georgesboris@gmail.com>
2021-02-05 04:26:02 -0300

    feat: improve close icon

--- src/UIBook/Widgets/Header.elm
+++ src/UIBook/Widgets/Header.elm
@@ -4,7 +4,7 @@ import Css exposing (..)
 import Html.Styled exposing (..)
 import Html.Styled.Attributes exposing (..)
 import Html.Styled.Events exposing (..)
-import Svg.Styled exposing (path, svg)
+import Svg.Styled exposing (path, rect, svg)
 import Svg.Styled.Attributes
 import UIBook.Widgets.Helpers exposing (..)
 
@@ -106,5 +106,23 @@ closeIcon =
         , Svg.Styled.Attributes.height "20"
         , Svg.Styled.Attributes.viewBox "0 0 512 512"
         ]
-        [ path [ Svg.Styled.Attributes.d "M284.286 256.002L506.143 34.144c7.811-7.811 7.811-20.475 0-28.285-7.811-7.81-20.475-7.811-28.285 0L256 227.717 34.143 5.859c-7.811-7.811-20.475-7.811-28.285 0-7.81 7.811-7.811 20.475 0 28.285l221.857 221.857L5.858 477.859c-7.811 7.811-7.811 20.475 0 28.285a19.938 19.938 0 0014.143 5.857 19.94 19.94 0 0014.143-5.857L256 284.287l221.857 221.857c3.905 3.905 9.024 5.857 14.143 5.857s10.237-1.952 14.143-5.857c7.811-7.811 7.811-20.475 0-28.285L284.286 256.002z" ] []
+        [ path [ Svg.Styled.Attributes.d "M451.792 0H59.208C26.561 0 0 26.561 0 59.208v393.084C0 484.939 26.561 511.5 59.208 511.5h392.584c32.647 0 59.208-26.561 59.208-59.208V59.208C511 26.561 484.439 0 451.792 0zM471 452.292c0 10.591-8.617 19.208-19.208 19.208H59.208C48.617 471.5 40 462.883 40 452.292V59.208C40 48.617 48.617 40 59.208 40h392.584C462.383 40 471 48.617 471 59.208v393.084z" ] []
+        , rect
+            [ Svg.Styled.Attributes.x "105"
+            , Svg.Styled.Attributes.y "377.943"
+            , Svg.Styled.Attributes.width "386"
+            , Svg.Styled.Attributes.height "40"
+            , Svg.Styled.Attributes.rx "20"
+            , Svg.Styled.Attributes.transform "rotate(-45 105 377.943)"
+            ]
+            []
+        , rect
+            [ Svg.Styled.Attributes.x "133.284"
+            , Svg.Styled.Attributes.y "105"
+            , Svg.Styled.Attributes.width "386"
+            , Svg.Styled.Attributes.height "40"
+            , Svg.Styled.Attributes.rx "20"
+            , Svg.Styled.Attributes.transform "rotate(45 133.284 105)"
+            ]
+            []
         ]
